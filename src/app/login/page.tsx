3ecb70d2b0936411bff1a7e45cf00e97dc695354
gulshan07dev <gulshanku9135@gmail.com>
2023-09-09 14:12:58 +0530

    added sendMail and verifyEmail

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,15 +40,15 @@ export default function LoginPage() {
   };
 
   return (
-    <section className="flex flex-col pt-12 min-h-screen w-screen bg-gray-950 overflow-x-hidden  items-center">
+    <section className="flex flex-col pt-7 min-h-screen w-screen gap-10 pb-5  overflow-x-hidden  items-center">
       {/* header */}
-      <header className="relative flex flex-col gap-3 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute  z-[0] after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] ">
+      <header className="relative flex flex-col gap-3 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute  z-[0] after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-3xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] ">
         <h1 className="md:text-5xl text-4xl text-orange-500 p-1 font-semibold text-center z-[2]">
           Welcome Again,
         </h1>
 
-        <h2 className="md:text-5xl text-4xl text-yellow-500 p-1 font-semibold text-center z-[2]">
-          <span className="text-purple-500">Login</span> Page
+        <h2 className="md:text-5xl text-4xl text-gray-800 p-1 font-semibold text-center z-[2]">
+          <span className="text-purple-600">Login</span> Page
         </h2>
       </header>
 
@@ -56,18 +56,18 @@ export default function LoginPage() {
       <form
         autoComplete="off"
         onSubmit={onLogin}
-        className="py-14 px-5 flex flex-col gap-6 bg-[#00000018] backdrop-blur-sm  border-purple-700 rounded-lg max-w-[500px]  w-[97%]"
+        className="py-12 px-7 flex flex-col gap-6 bg-[#00000018] backdrop-blur-sm  border-purple-700 rounded-lg max-w-[500px]  w-[95%]"
       >
         {/* email */}
         <div className="flex flex-col gap-1.5 w-[100%]">
           <label
             htmlFor="email"
-            className='text-2xl text-white font-medium after:content-["*"] after:text-red-400'
+            className='text-2xl text-gray-900 font-medium after:content-["*"] after:text-red-400'
           >
             Email
           </label>
           <input
-            className="text-white md:text-lg text-base w-[100%] py-3.5 px-3 transition-all rounded-xl dark:bg-neutral-800/50 outline-none focus:dark:bg-neutral-800/30 focus:border-gray-500 border-2 border-blue-800"
+            className="text-gray-600 md:text-lg text-base w-[100%] py-3.5 px-3 transition-all rounded-xl bg-gray-50 outline-none focus:dark:bg-white focus:border-gray-500 border-2  "
             type="email"
             id="email"
             placeholder="Enter your email..."
@@ -79,12 +79,12 @@ export default function LoginPage() {
         <div className="flex flex-col gap-1.5 w-[100%]">
           <label
             htmlFor="password"
-            className='text-2xl text-white font-medium after:content-["*"] after:text-red-400'
+            className='text-2xl text-gray-900 font-medium after:content-["*"] after:text-red-400'
           >
             Password
           </label>
           <input
-            className="text-white md:text-lg text-base w-[100%] py-3.5 px-3 transition-all rounded-xl dark:bg-neutral-800/50 outline-none focus:dark:bg-neutral-800/30 focus:border-gray-500 border-2 border-blue-800"
+           className="text-gray-600 md:text-lg text-base w-[100%] py-3.5 px-3 transition-all rounded-xl bg-gray-50 outline-none focus:dark:bg-white focus:border-gray-500 border-2  "
             type="password"
             id="password"
             placeholder="Enter your password..."
@@ -101,10 +101,10 @@ export default function LoginPage() {
         </button>
 
         <div>
-          <span className="text-gray-400 text-sm font-medium">
+          <span className="text-gray-600 text-sm font-medium">
             if you don't have a account?{" "}
           </span>
-          <Link href={"/signup"}>Signup</Link>
+          <Link href={"/signup"} className="text-blue-600 font-[500]">Signup</Link>
         </div>
       </form>
     </section>
